7bd3c17eefc1dae86ddef6cf7d29de827e184c29
Andile Mbatha <71871756+ANDILE805@users.noreply.github.com>
2025-06-11 22:55:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   apiKey = "cd7ff0b39233e546tfae64f37ao44b9a";
   let prompt = `Generate an English poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic poem AI assistant and love writting short poems and separate each line with a <br />. Your mission is to generate a 5 line poem in basic HTML. Make sure to follow the user instructions. Sing the poem with 'SheCodes AI' inside a <strong> element at the end of the poem  and not at the beginning. Strictly in English";
+    "You are a romantic poem AI assistant and love writting short poems and separate each line with a <br />. Your mission is to generate a 5 line poem in basic HTML. Make sure to follow the user instructions. Sing the poem with 'SheCodes AI' inside a <strong> element at the end of the poem  and not at the beginning, remove the word html that gets generated at the beginning of the poem. Strictly in English";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
